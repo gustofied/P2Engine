fd39e266da17cd9dbcb94b3c883f72bf1a7b24bc
Adam S <134907338+adam-sioud@users.noreply.github.com>
2024-10-12 21:31:04 +0200

    Added: uhm we need to fix this, but now it's actually starting

---
+++ projectdeux/daml.js/projectdeux-0.1.0/lib/index.d.ts
@@ -0,0 +1,5 @@
+import * as Setup from './Setup';
+export { Setup } ;
+import * as User from './User';
+export { User } ;
+export declare const packageId = '037ed5a3c8e5ddb25d1008fbe4fd7f246421b9f0e1dd10ca118812a42b53621c';
